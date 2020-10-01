b46d38cc90e7d478cd3a5a0c4aeb22a359d6656b
Zach Waggoner <zach.waggoner@zuora.com>
2020-10-01 13:18:20 -0700

    update files

---
+++ hello-world/src/components/body.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { render } from "react-dom";
+
+function Welcome(props) {
+    return <h1>Hello World</h1>;
+}
+
+ReactDOM.render(Welcome, document.getElementById('root'));
+
--- hello-world/src/pages/about.js
+++ hello-world/src/pages/about.js
@@ -1,8 +1,11 @@
 import React from "react"
 import Header from "../components/header"
+import Header from "../components/body"
 export default () => (
   <div style={{ color: `teal` }}>
     <Header headerText="About Gatsby" />
     <p>Such wow. Very React.</p>
+    <Body />
   </div>
+
 )
